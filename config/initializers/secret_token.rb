c2fe2c62e53f8c251a64b07468d526aed7293674
vishnuvn <vishnun@qburst.com>
2013-10-29 17:20:47 +0530

    added new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyGemBlogger::Application.config.secret_token = 'c260c144753cd05a21898aaeb5516fb22cd39c5bfa7364151ba914d5ef773c8fc5d2b66e786bddd06112845e1aa04dc3e74b52c80befa50f328ad290beb3721e'
